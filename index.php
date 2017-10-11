f81e60de8eed44aeb2cbfdc4eaf95acc4f8eeb0f
Nahuel Rapetti <n.rapetti@hotmail.com>
2017-10-10 22:04:11 -0300

    Primer Commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once('php/header.php');
+
+?>
+
+    <div>
+        <p>Bienvenido!</p>
+    </div>
+
+<?php
+require_once('php/footer.php');
+?>
---
+++ php/header.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>TP Web 2</title>
+        <script type="text/javascript" src="/resources/js/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="/resources/js/materialize.min.js"></script>
+        <link rel="stylesheet" href="/resources/css/materialize.min.css">
+        <link rel="stylesheet" href="/resources/css/style.css">
+    </head>
+    <body>
+        <header>
+            <nav class="blue darken-1">
+                <div class="nav-wrapper container">
+                <a href="#" class="brand-logo right">Logo</a>
+                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">JavaScript</a></li>
+                </ul>
+                </div>
+            </nav>
+        </header>
+        <main class="container">
