e56882356786b951e1948b719f1fdbff83d0be75
Nahuel Rapetti <n.rapetti@hotmail.com>
2017-10-17 16:16:45 -0300

    imagenes y favicon

--- php/header.php
+++ php/header.php
@@ -3,6 +3,7 @@
         <title>Log&iacute;stica NAUMAR</title>
         <script type="text/javascript" src="/resources/js/jquery-3.2.1.min.js"></script>
         <script type="text/javascript" src="/resources/js/materialize.min.js"></script>
+        <link rel="shortcut icon" type="image/png" href="/resources/img/truck16.png"/>
         <link rel="stylesheet" href="/resources/css/materialize.min.css">
         <link rel="stylesheet" href="/resources/css/style.css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
